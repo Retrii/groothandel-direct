254abb3a8dac0fe86379037c81f5af91ae1d3d51
Joey Both <joeyboth@gmail.com>
2025-08-29 17:08:57 +0200

    refactor: simplify Items component structure and improve styling
    - Removed the Table component for a more straightforward layout.
    - Added spacing between items for better visual separation.
    - Maintained sorting and rendering logic for items and skeletons.

--- storefront/src/modules/order/components/items/index.tsx
+++ storefront/src/modules/order/components/items/index.tsx
@@ -1,6 +1,5 @@
 import repeat from "@/lib/util/repeat"
 import { HttpTypes } from "@medusajs/types"
-import { Table } from "@medusajs/ui"
 
 import Item from "@/modules/order/components/item"
 import SkeletonLineItem from "@/modules/skeletons/components/skeleton-line-item"
@@ -12,22 +11,18 @@ type ItemsProps = {
 
 const Items = ({ items, order }: ItemsProps) => {
   return (
-    <div className="flex flex-col">
-      <Table>
-        <Table.Body data-testid="artikelen-table" className="border-none">
-          {items?.length
-            ? items
-                .sort((a, b) => {
-                  return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
-                })
-                .map((item) => {
-                  return <Item key={item.id} item={item} order={order} />
-                })
-            : repeat(5).map((i) => {
-                return <SkeletonLineItem key={i} />
-              })}
-        </Table.Body>
-      </Table>
+    <div className="flex flex-col space-y-4" data-testid="artikelen-table">
+      {items?.length
+        ? items
+            .sort((a, b) => {
+              return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
+            })
+            .map((item) => {
+              return <Item key={item.id} item={item} order={order} />
+            })
+        : repeat(5).map((i) => {
+            return <SkeletonLineItem key={i} />
+          })}
     </div>
   )
 }
